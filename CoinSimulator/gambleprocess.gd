37cf5f5671bfa50fa5938c3ac5fc7e43cb9dce52
lostlp09 <mrnootnoot09@gmail.com>
2025-10-03 11:14:28 +0200

    added decorationslot (will work in the future)

--- CoinSimulator/gambleprocess.gd
+++ CoinSimulator/gambleprocess.gd
@@ -1,6 +1,8 @@
 extends Button
 
 @onready var amounttextobject =$"../Label"
+@onready var  allslots = $"../allslots"
+var Gambling:bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.pressed.connect(gamblingactivated)
@@ -23,7 +25,32 @@ func gamblingactivated() -> void:
 		else:
 			Money.money+= amountyouwetted
 			print("you won the jackpot")
+
+
+func _process(delta: float) -> void:
+	if  Gambling == false:
+		for i in allslots.get_children():
+			i.position.y += 0.5
+			if i.position.y >= -45 :
+				var newclone = i.duplicate()
+				if i.is_in_group("firstslot"):
+					
+					newclone.position = Vector2(-82.937,-438.563)
+				
+				elif  i.is_in_group("secondslot"):
+					newclone.position = Vector2(0.0,-438.563)
+					
+				else:
+					newclone.position = Vector2(81.063,-438.563)
+				allslots.add_child(newclone)
+				i.queue_free()
+				
+
+
 			
+				
+				
+
 			
 			
 		
